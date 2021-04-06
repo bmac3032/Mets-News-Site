4fbd0840cdd63123b2ee97f3b7fdefad478546eb
Brian McPherson <heat3032@gmail.com>
2021-04-06 13:43:53 -0400

    Basic functionality setup, needs to implement 1. JSON with player data. 2. URL Parameters so we can show specific player inforamtion and stats (component) based on a variable/parameter passed to a URL

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import { Route, BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Nascar Project</h1>
+      <Router>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+      </Router>
     </div>
   );
 }
