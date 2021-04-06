5d7d1ddfe2ce29346f2b10a20f6aa6d1c56fa015
Brian McPherson <heat3032@gmail.com>
2021-04-06 16:00:14 -0400

    Added Bootstrap and basic styling. Still needs Copy and JSON for Players, as well as functionality for Player Componenets, and for the data to be passed to the Component to properly display their information

--- src/App.js
+++ src/App.js
@@ -1,15 +1,49 @@
 import "./App.css";
 import Home from "./Components/Home";
 import About from "./Components/About";
-import { Route, BrowserRouter as Router } from "react-router-dom";
+import {
+  Route,
+  Link,
+  Switch,
+  NavLink,
+  BrowserRouter as Router,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>Nascar Project</h1>
       <Router>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/about" component={About} />
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+          <a className="navbar-brand">New York Mets</a>
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <NavLink
+                  exact
+                  to="/"
+                  activeClassName="active"
+                  className="nav-link"
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  exact
+                  activeClassName="active"
+                  to="/about"
+                  className="nav-link"
+                >
+                  About
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+        </Switch>
       </Router>
     </div>
   );
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,8 +2,9 @@ import React from "react";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="container text-center">
+      <h1>Let's Go Mets!</h1>
+      <p>Your one stop shop for all things Mets news and updates!</p>
     </div>
   );
 }
