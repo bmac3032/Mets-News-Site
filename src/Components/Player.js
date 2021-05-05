3a764eaeac4da18b476e1abc45aae42faca1f9ae
Brian McPherson <heat3032@gmail.com>
2021-05-05 13:23:09 -0400

    latest update

--- src/Components/Player.js
+++ src/Components/Player.js
@@ -1,16 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Player({ name, position, number, img, key }) {
   return (
     <div className="col-3">
-      <div className="card mt-3 mb-3">
-        <img src={img} className="card-img-top" alt={`Card showing ${name}`} />
-        <div className="card-body">
-          <h5 className="card-title">{name}</h5>
-          <p className="card-text">{`Position: ${position}`}</p>
-          <p className="card-text">{`Number: ${number}`}</p>
+      <Link to={`/players/${name}`}>
+        <div className="card mt-3 mb-3">
+          <img
+            src={img}
+            className="card-img-top"
+            alt={`Card showing ${name}`}
+          />
+          <div className="card-body">
+            <h5 className="card-title">{name}</h5>
+            <p className="card-text">{`Position: ${position}`}</p>
+            <p className="card-text">{`Number: ${number}`}</p>
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 }
